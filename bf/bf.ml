f9a98deccbd6b42fabdda28b155b2a2b74e6a9c3
dforgeas <david.forgeas@gmail.com>
2014-05-26 21:39:13 +0200

    Revert "bf.ml: update the parser: change the result type to a command array instead of a command list"
    This reverts commit 62a8511179acc8316a1a0ebadd90b1a0feb3ca13.

--- bf/bf.ml
+++ bf/bf.ml
@@ -1,26 +1,15 @@
 type bFCommand = GoRight | GoLeft | Increment | Decrement | Print | Read | While | Wend
-	| Loop of bFSource | IncrementN of int | GoRightN of int | GoLeftN of int | SetZero
-and bFSource = bFCommand array
+	| Loop BFSource | IncrementN of int | GoRightN of int | GoLeftN of int | SetZero
+and bFSource = bFCommand list
 
-let stringMap f str =
-	let len = String.length str in
-	let res = Array.make len (f '\x00') in
-	for i = 0 to len - 1 do
-		res.(i) <- f str.[i]
-	done;
-	res
+let rec mapOptions f = function
+	| [] -> []
+	| x :: xs -> let rest = mapOptions f in
+		match f x with
+		| None -> rest
+		| Some e -> e :: rest
 
-let arrayDestructiveFilter p t arr =
-	let i_res = ref 0 in
-	for i_src = 0 to Array.length arr - 1 do
-		if p arr.(i_src) then begin
-			arr.(!i_res) <- arr.(i_src);
-			incr i_res
-		end
-	done;
-	Array.map t (Array.sub arr 0 !i_res) (* truncate and transform the result *)
-
-let charToBF = function
+and charToBF = function
 	| '>' -> Some GoRight
 	| '<' -> Some GoLeft
 	| '+' -> Some Increment
@@ -31,12 +20,9 @@ let charToBF = function
 	| ']' -> Some Wend
 	| _ -> None
 
-let parseBrainfuck source =
-	let notNone = function
-		| None -> false
-		| Some _ -> true
-	and extractSome = function
-		| Some x -> x
-		| None -> failwith "Unexpected None value in extractSome function"
-	in
-	arrayDestructiveFilter notNone extractSome (stringMap charToBF source)
+and parseBrainfuck source =
+	let result = ref [] in
+	for i = String.length source - 1 downto 0 do
+		result := source.[i] :: !result
+	done;
+	mapOptions charToBF !result
